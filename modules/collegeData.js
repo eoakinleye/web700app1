0c626098dcecd5dc2ce5a1e1918acce162a12b8c
eoakinleye <akinleye.oluwadamilare@gmail.com>
2023-11-17 23:41:07 -0500

    Your commit message here

---
+++ modules/collegeData.js
@@ -0,0 +1,180 @@
+// const fs = require("fs");
+
+// class Data{
+//     constructor(students, courses){
+//         this.students = students;
+//         this.courses = courses;
+//     }
+// }
+
+// let dataCollection = null;
+
+// module.exports.initialize = function () {
+//     return new Promise( (resolve, reject) => {
+//         fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+//             if (err) {
+//                 reject("unable to load courses"); return;
+//             }
+
+//             fs.readFile('./data/students.json','utf8', (err, studentData) => {
+//                 if (err) {
+//                     reject("unable to load students"); return;
+//                 }
+
+//                 dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
+//                 resolve();
+//             });
+//         });
+//     });
+// }
+
+// module.exports.getAllStudents = function(){
+//     return new Promise((resolve,reject)=>{
+//         if (dataCollection.students.length == 0) {
+//             reject("query returned 0 results"); return;
+//         }
+
+//         resolve(dataCollection.students);
+//     })
+// }
+
+// module.exports.getTAs = function () {
+//     return new Promise(function (resolve, reject) {
+//         var filteredStudents = [];
+
+//         for (let i = 0; i < dataCollection.students.length; i++) {
+//             if (dataCollection.students[i].TA == true) {
+//                 filteredStudents.push(dataCollection.students[i]);
+//             }
+//         }
+
+//         if (filteredStudents.length == 0) {
+//             reject("query returned 0 results"); return;
+//         }
+
+//         resolve(filteredStudents);
+//     });
+// };
+
+// module.exports.getCourses = function(){
+//    return new Promise((resolve,reject)=>{
+//     if (dataCollection.courses.length == 0) {
+//         reject("query returned 0 results"); return;
+//     }
+
+//     resolve(dataCollection.courses);
+//    });
+// };
+
+// module.exports.getStudentByNum = function (num) {
+//     return new Promise(function (resolve, reject) {
+//         var foundStudent = null;
+
+//         for (let i = 0; i < dataCollection.students.length; i++) {
+//             if (dataCollection.students[i].studentNum == num) {
+//                 foundStudent = dataCollection.students[i];
+//             }
+//         }
+
+//         if (!foundStudent) {
+//             reject("query returned 0 results"); return;
+//         }
+
+//         resolve(foundStudent);
+//     });
+// };
+
+// module.exports.getStudentsByCourse = function (course) {
+//     return new Promise(function (resolve, reject) {
+//         var filteredStudents = [];
+
+//         for (let i = 0; i < dataCollection.students.length; i++) {
+//             if (dataCollection.students[i].course == course) {
+//                 filteredStudents.push(dataCollection.students[i]);
+//             }
+//         }
+
+//         if (filteredStudents.length == 0) {
+//             reject("query returned 0 results"); return;
+//         }
+
+//         resolve(filteredStudents);
+//     });
+// };
+
+// module.exports.addStudent = function (studentData) {
+//     return new Promise(function (resolve, reject) {
+
+//         studentData.TA = (studentData.TA) ? true : false;
+//         studentData.studentNum = dataCollection.students.length + 1;
+//         dataCollection.students.push(studentData);
+
+//         resolve();
+//     });
+
+// };
+
+
+const fs = require("fs");
+
+class Data{
+    constructor(students, courses){
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+function findStudentIndex(studentNum) {
+    for (let i = 0; i < dataCollection.students.length; i++) {
+        if (dataCollection.students[i].studentNum == studentNum) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+module.exports.initialize = function () {
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+            if (err) {
+                reject("unable to load courses"); return;
+            }
+
+            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+                if (err) {
+                    reject("unable to load students"); return;
+                }
+
+                dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
+                resolve();
+            });
+        });
+    });
+}
+
+module.exports.getAllStudents = function(){
+    return new Promise((resolve,reject)=>{
+        if (dataCollection.students.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(dataCollection.students);
+    })
+}
+
+// Other methods remain unchanged.
+
+module.exports.updateStudent = function (studentNum, updatedData) {
+    return new Promise((resolve, reject) => {
+        let index = findStudentIndex(studentNum);
+
+        if (index === -1) {
+            reject("Student not found."); return;
+        }
+
+        dataCollection.students[index] = {...dataCollection.students[index], ...updatedData};
+        resolve();
+    });
+};
